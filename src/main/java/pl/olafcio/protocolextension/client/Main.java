f068a05f598bd96b0d9f38aeed60a84a78978a2a
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-11-27 23:40:38 +0100

    [UP] Further work on the packet magic it compiles now, but the packets don't work (Minecraft) Client disconnected with reason:             Internal Exception: io.netty.handler.codec.DecoderException:             Failed to decode packet 'clientbound/minecraft:custom_payload'

--- src/main/java/pl/olafcio/protocolextension/client/Main.java
+++ src/main/java/pl/olafcio/protocolextension/client/Main.java
@@ -21,6 +21,7 @@
 
 package pl.olafcio.protocolextension.client;
 
+import com.mojang.datafixers.util.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
@@ -44,13 +45,13 @@ import pl.olafcio.protocolextension.client.state.WindowTitle;
 import pl.olafcio.protocolextension.client.state.hud.HudElement;
 import pl.olafcio.protocolextension.client.state.hud.HudState;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Main implements ModInitializer, ClientModInitializer {
@@ -67,23 +68,34 @@ public class Main implements ModInitializer, ClientModInitializer {
 
         // S2C
         try {
-            addS2CPacket(ActivateS2CPayload.class, ActivateS2CPayload.ID);
-            addS2CPacket(HUDToggleS2CPayload.class, HUDToggleS2CPayload.ID);
-            addS2CPacket(HUDPutElementS2CPayload.class, HUDPutElementS2CPayload.ID);
-            addS2CPacket(HUDDeleteElementS2CPayload.class, HUDDeleteElementS2CPayload.ID);
-            addS2CPacket(HUDClearS2CPayload.class, HUDClearS2CPayload.ID);
-            addS2CPacket(SetWindowTitleS2CPayload.class, SetWindowTitleS2CPayload.ID);
-            addS2CPacket(ServerCommandS2CPayload.class, ServerCommandS2CPayload.ID);
-            addS2CPacket(MoveToggleS2CPayload.class, MoveToggleS2CPayload.ID);
-            addS2CPacket(HUDSettingHotbarS2CPayload.class, HUDSettingHotbarS2CPayload.ID);
+            addPacket(ActivateS2CPayload.class, ActivateS2CPayload.ID).registerS2C();
+            addPacket(HUDToggleS2CPayload.class, HUDToggleS2CPayload.ID).registerS2C();
+            addPacket(HUDPutElementS2CPayload.class, HUDPutElementS2CPayload.ID).registerS2C();
+            addPacket(HUDDeleteElementS2CPayload.class, HUDDeleteElementS2CPayload.ID).registerS2C();
+            addPacket(HUDClearS2CPayload.class, HUDClearS2CPayload.ID).registerS2C();
+            addPacket(SetWindowTitleS2CPayload.class, SetWindowTitleS2CPayload.ID).registerS2C();
+            addPacket(ServerCommandS2CPayload.class, ServerCommandS2CPayload.ID).registerS2C();
+            addPacket(MoveToggleS2CPayload.class, MoveToggleS2CPayload.ID).registerS2C();
+            addPacket(HUDSettingHotbarS2CPayload.class, HUDSettingHotbarS2CPayload.ID).registerS2C();
         } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException("Failed to add S2C packets", e);
         }
     }
 
+    record Unbound<T>(Method method) implements Function<T, Object> {
+        @Override
+        @SuppressWarnings("unchecked")
+        public T apply(Object o) {
+            try {
+                return (T) this.method.invoke(o);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Failed to apply unbound", e);
+            }
+        }
+    }
+
     @SuppressWarnings("unchecked")
-    private <T extends Record> void addS2CPacket(Class<T> record, UIdentifier uid) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        var id = Identifier.of(uid.namespace(), uid.path());
+    private <T extends Record> PayloadRecord<?> addPacket(Class<T> record, UIdentifier uid) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         PacketCodec<RegistryByteBuf, T> codec;
 
         var values = record.getRecordComponents();
@@ -91,8 +103,8 @@ public class Main implements ModInitializer, ClientModInitializer {
             var paramTypes = new ArrayList<Class<?>>();
             var paramValues = new ArrayList<>();
             for (var fComponent : values) {
-                var fCodec = getPacketCodec(fComponent);
-                var fFunction = MethodHandles.lookup().unreflect(fComponent);
+                var fCodec = getPacketCodec(fComponent.getType());
+                var fFunction = new Unbound(fComponent.getAccessor());
 
                 paramTypes.add(PacketCodec.class);
                 paramTypes.add(Function.class);
@@ -101,33 +113,35 @@ public class Main implements ModInitializer, ClientModInitializer {
                 paramValues.add(fFunction);
             }
 
-            paramTypes.add(Function.class);
-            paramValues.add(record.getDeclaredConstructors()[0]);
+            passConstructor(record.getDeclaredConstructors()[0], paramTypes, paramValues);
 
             var method = getMethod(paramTypes);
             var args = paramValues.toArray(Object[]::new);
 
             codec = (PacketCodec<RegistryByteBuf, T>)
-                    method.invoke(null, (Object[]) args);
+                    method.invoke(null, args);
         } else {
             codec = PacketCodec.unit(record.getDeclaredConstructor().newInstance());
         }
 
-        var plays2c = PayloadTypeRegistry.playS2C();
-        var register = plays2c.getClass().getDeclaredMethod("register", CustomPayload.Id.class, PacketCodec.class);
-
+        var id = Identifier.of(uid.namespace(), uid.path());
         var cpID = new CustomPayload.Id<>(id);
-        packet2id.put(record, cpID);
 
-        register.invoke(
-                plays2c,
-                cpID,
-                codec
-        );
+        var pr = new PayloadRecord(cpID, codec);
+        payloads.put(record.getName(), pr);
+        return pr;
     }
 
-    private final HashMap<Class<? extends Record>, CustomPayload.Id<?>> packet2id = new HashMap<>();
+    record PayloadRecord<T extends CustomPayload>(
+            CustomPayload.Id<T> cpID,
+            PacketCodec<RegistryByteBuf, T> codec
+    ) {
+        public void registerS2C() {
+            PayloadTypeRegistry.playS2C().register(cpID, codec);
+        }
+    }
 
+    private static final HashMap<String, PayloadRecord<?>> payloads = new HashMap<>();
     private static final HashMap<Class<?>, PacketCodec<?, ?>> typeMap = new HashMap<>(){{
         this.put(String.class, PacketCodecs.STRING);
         this.put(short.class, PacketCodecs.SHORT);
@@ -136,12 +150,10 @@ public class Main implements ModInitializer, ClientModInitializer {
         this.put(boolean.class, PacketCodecs.BOOLEAN);
     }};
 
-    private static @NotNull PacketCodec<?, ?> getPacketCodec(Method fMethod) {
-        var type = fMethod.getReturnType();
-
+    private static @NotNull PacketCodec<?, ?> getPacketCodec(Class<?> type) {
         var fCodec = typeMap.get(type);
         if (fCodec == null)
-            throw new RuntimeException("'null' type encountered; cannot get java type's codec");
+            throw new RuntimeException("'" + type.getName() + "' type encountered; cannot get java type's codec");
 
         return fCodec;
     }
@@ -160,15 +172,68 @@ public class Main implements ModInitializer, ClientModInitializer {
         }
 
         if (method == null)
-            throw new RuntimeException("Couldn't construct S2C packet: failed to retrieve appropriate tuple(...) variant");
+            throw new RuntimeException("Couldn't construct PX packet: failed to retrieve appropriate tuple(...) variant");
 
+        Main.logger.info("Found {}", method);
         return method;
+    }
+
+    @FunctionalInterface
+    interface VarFunction<T> {
+        T apply(Object... varargs);
+    }
+
+    private static <T> void passConstructor(Constructor<T> constructor, ArrayList<Class<?>> paramTypes, ArrayList<Object> paramValues) {
+        var params = constructor.getParameterCount();
+        var callback = (VarFunction<T>) varargs -> {
+            try {
+                return constructor.newInstance(varargs);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Failed to construct payload", e);
+            }
+        };
+
+        // TODO: Reflectively shorten this code with a Map<int [parameterCount], ? super Function [callback]>
+        if (params == 1) {
+            paramTypes.add(Function.class);
+            paramValues.add((Function<?, ?>) callback::apply);
+        } else if (params == 2) {
+            paramTypes.add(BiFunction.class);
+            paramValues.add((BiFunction<?, ?, ?>) callback::apply);
+        } else if (params == 3) {
+            paramTypes.add(Function3.class);
+            paramValues.add((Function3<?, ?, ?, ?>) callback::apply);
+        } else if (params == 4) {
+            paramTypes.add(Function4.class);
+            paramValues.add((Function4<?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 5) {
+            paramTypes.add(Function5.class);
+            paramValues.add((Function5<?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 6) {
+            paramTypes.add(Function6.class);
+            paramValues.add((Function6<?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 7) {
+            paramTypes.add(Function7.class);
+            paramValues.add((Function7<?, ?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 8) {
+            paramTypes.add(Function8.class);
+            paramValues.add((Function8<?, ?, ?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 9) {
+            paramTypes.add(Function9.class);
+            paramValues.add((Function9<?, ?, ?, ?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 10) {
+            paramTypes.add(Function10.class);
+            paramValues.add((Function10<?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        } else if (params == 11) {
+            paramTypes.add(Function11.class);
+            paramValues.add((Function11<?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?>) callback::apply);
+        }
     }
 
     @SuppressWarnings("unchecked")
     private <T extends Record> void handleS2C(Class<T> packet, BiConsumer<T, ClientPlayNetworking.Context> handler) {
-        var id = packet2id.get(packet);
-        ClientPlayNetworking.registerGlobalReceiver(id, (payload, context) -> {
+        var pr = payloads.get(packet.getName());
+        ClientPlayNetworking.registerGlobalReceiver(pr.cpID, (payload, context) -> {
             context.client().execute(() -> handler.accept((T) payload, context));
         });
     }
